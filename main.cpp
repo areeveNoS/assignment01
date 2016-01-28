cf5c20e205a262300ecc0b4c7398c89590f06542
areeveNoS <areeve.nos@gmail.com>
2016-01-28 15:17:41 -0800

    Create main.cpp
    source file for assignment 01

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Ben Blazak <bblazak@fullerton.edu>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* A program that converts integers into roman numerals and outputs it.
+*/
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{	
+	//Declarations
+	int n;
+	cout << "Please enter a number: ";
+	cin >> n;
+
+
+}
+
